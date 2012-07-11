6c5761e5e5260924d19bae50cad0a73f909a088b
unknown <m.constantinescu@isbc-w64.netig.local>
2012-07-11 09:20:12 +0300

    - added flags for services

--- tbird-sign.py
+++ tbird-sign.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env python
 import tweepy
 
-print :e 
+
+#Google Plus
+useGooglePlus = 1
+googleName = mariuscristianc
+#Tweeter
+useTweeter = 1
+tweeterName = mariuscristianc
+#Linkedin
+useLinkedin = 1
+linkedinName = mariuscristianc
+#identica
+useIdentica = 1
+identicaName = mariuscristianc
+#facebook
+useFacebook = 1
+facebookName = mariuscristianc
+#stack overflow
+useStackOverflow = 1
+stackOverflowName = mariuscristianc
+#pinterest
+#wordpress
+#blogger
+#rss
+#etsy
+#ebay
+#github
+#digg
+#quora
+#
+
+
 print tweepy.api.user_timeline('mariuscristianc')[0].text
 
